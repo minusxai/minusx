f6db26587ecd82f85edc99e6d065294bed834c9a
Vivek Aithal <vivekaithal44@gmail.com>
2026-04-28 14:04:20 -0700

    static bug (#289)

--- frontend/components/connection-wizard/steps/StepConnection.tsx
+++ frontend/components/connection-wizard/steps/StepConnection.tsx
@@ -48,6 +48,7 @@ export default function StepConnection({ onComplete, onStaticSelect, greeting }:
         skipTypePicker
         onSaveSuccess={onComplete}
         hideCancel
+        onStaticSelect={onStaticSelect}
       />
     );
   }
